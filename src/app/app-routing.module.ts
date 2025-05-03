833dd49ad85a2e5ed8a1b15cd4f72810303f2b2d
anjan-in <anjansen30@gmail.com>
2025-05-04 00:21:14 +0530

    Configured GitHub Pages deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthGuard } from './guards/auth.guard';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { LoginComponent } from './pages/login/login.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'profile', component: ProfileComponent },
-  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule), canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '/login', pathMatch: 'full' }  // Default route to home page if no other routes match
-];
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
