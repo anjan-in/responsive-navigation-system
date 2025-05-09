ba2669acab9f863f304dc103105259e888c6c4a7
anjan-in <anjansen30@gmail.com>
2025-05-09 11:49:24 +0530

    Added user-form component to create new user

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'profile', component: ProfileComponent },
-    { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+    { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
     { path: 'user-management', loadChildren: () => import('./pages/user-management/user-management.module').then(m => m.UserManagementModule), canActivate: [AuthGuard] },
     { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule), canActivate: [AuthGuard] },
     { path: '**', redirectTo: '/login', pathMatch: 'full' }  // Default route to home page if no other routes match
